a6c8e85ff13c177cd9d032ebed098f3fe8a7ef82
zm <562697599@qq.com>
2023-06-11 15:11:40 +0800

    初步实现移动笔记时判断附件并进行移动，更新笔记中对附件的引用链接。

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,199 @@
-import { Plugin } from "obsidian"
-import { VueSamplePluginSettingTab } from "./setting"
+import { Plugin, TAbstractFile ,DataAdapter ,MarkdownView, TFile, Notice } from "obsidian"
+import { AttachmentsBindsSettingTab,AttachmentsBindsSettingsType,attachmentsBindsSettings } from "./setting"
+import path from 'path';
 
-export default class VueSamplePlugin extends Plugin {
-    onload() {
-        this.addSettingTab(new VueSamplePluginSettingTab(this.app, this))
+
+
+// 注册插件
+export default class AttachmentsBindsPlugin extends Plugin {
+    settings: AttachmentsBindsSettingsType=attachmentsBindsSettings;
+    
+     // 在用户激活插件和插件更新时触发，这将是您设置插件大部分功能的地方
+     async onload() {
+        await this.loadSettings();
+        // 加入插件设置页
+        this.addSettingTab(new AttachmentsBindsSettingTab(this.app, this))
+        this.settings = Object.assign(
+			{},
+			attachmentsBindsSettings,
+			await this.loadData()
+		);
+        console.log('----------加载的配置内容：------------');
+        console.log(this.settings);
+
+        this.registerEvent(this.app.vault.on('rename', async (file: TAbstractFile, oldPath: string) => {
+            console.log('触发了rename事件');
+            console.log('移动后的笔记路径：'+file.path);
+            console.log('移动前的路经：'+oldPath);
+            const new_att_base = file.parent.path + '/' + this.settings.attachmentDir;
+            const sepraterIndex = oldPath.lastIndexOf("/");
+            const parentPath = oldPath.substring(0,sepraterIndex);//获取原路径
+            const dataAdapter = this.app.vault.adapter;
+            const oldChildren = await dataAdapter.list(parentPath);
+            // console.log(oldChildren);
+            // oldChildren.folders;
+            //获取源路径下所有的文件夹，例如：0:"01-性能测试/attachments"
+            //todo 声明一个集合（集合是k-v结构，k是附件名，v是路径），记录移动过的附件名称，集合中存在的就是已经移动过的，不在移动，只替换名称
+            let handleAttMap = new Map();//已处理的附件集合
+            const attachmentPath: string[] = [];
+            for (const item of oldChildren.folders) {
+                if (item.contains(this.settings.attachmentDir)) {
+                    attachmentPath.push(item);
+                }
+            }
+            
+            const  tf = this.app.vault.getAbstractFileByPath(file.path);
+            if(tf!= null&& tf instanceof TFile){
+                // const tFile = new TFile(tf.path, tf.stat);
+                const content = await this.app.vault.read(tf);
+                console.log('笔记内容-->');
+                console.log(content);
+                const attachments = await  this.findAttachments(content);
+                // console.log(attachments);
+                //附件路径
+                // const attachmentPath = oldPath.substring(0,oldPath.lastIndexOf('/'));
+                const att_all = (await dataAdapter.list(attachmentPath[0])).files;
+                attachments.forEach( item => {
+                    console.log('笔记附件：'+item);
+                    const item_name =  item.substring(item.lastIndexOf('/')+1,item.indexOf('|') > -1 ?item.indexOf('|'):item.length);
+                    // const item_name = item && item.indexOf('|') > -1 ? item.substring(0, item.indexOf('|')) : item;
+
+                    // const flag = this.isAbsolutPath(item);
+                    //不分绝对路径和相对路径了，直接判断这个附件的地址是不是在当前目录下有，有的话就进行处理，没有就不处理
+                    // if(att_all.includes(item)){
+                    //     this.moveAttachment();
+                    // };
+                    att_all.forEach(async att => {
+                        if(att.contains(item_name) && !handleAttMap.get(item_name)){
+                           const handleAttPath = await this.moveAttachment(att,(new_att_base+'/'+item_name),item_name);
+                           console.log('处理后的附件路径:%s',handleAttPath);
+                           const new_content = content.replaceAll(item,handleAttPath);//替换笔记中附件的引用地址
+                           console.log('替换后的笔记内容：%s',new_content);
+                           await this.app.vault.modify(tf, new_content);
+                           new Notice(`笔记 "${tf.name}" 成功更新！`);
+                           handleAttMap.set(item_name,handleAttPath);
+                        }
+                    });
+
+                    //修改笔记中附件的引用地址
+
+                });
+            }
+            
+        }));
+
+
+
     }
+    // 在用户禁用插件时触发，插件所调用的任何资源必须在这里得到释放，以防止 Obsidian 的性能受到影响
     onunload() {
     }
+    //保存配置
+    async saveSettings() {
+        await this.saveData(this.settings);
+        console.log('----------要保存的内容：------------');
+        console.log(this.settings);
+    }
+    //加载配置
+    async loadSettings() {
+        this.settings = Object.assign(
+            {},
+            attachmentsBindsSettings,
+            await this.loadData()
+        );
+    }
+
+    // 移动附件
+    async moveAttachment(sourcePath : string, destinationPath :string,attName:string){
+        console.log('附件的源路径：'+sourcePath);
+        console.log('附件的新路径：'+destinationPath);
+        console.log('附件名称：'+attName);
+        // 获取要移动的文件
+        const sourceFile = this.app.vault.getAbstractFileByPath(sourcePath) as TFile;
+        const destinationFile = this.app.vault.getAbstractFileByPath(destinationPath) as TFile;
+        // 如果要移动的文件存在
+        if (sourceFile) {
+
+            //如果目标文件存在相同名称附件
+            //todo 考虑笔记中多次应用一个附件，移动多次问题
+            if(destinationFile){
+                const attSubfix = attName.substring(attName.lastIndexOf('.'));
+                const attPrefix = attName.substring(0,attName.lastIndexOf('.'));
+                const new_attName = attPrefix+'_'+this.getDate()+attSubfix;
+                console.log('目标路径存在相同名称文件，重命名为：%s',new_attName);
+                destinationPath = destinationPath.replace(attName,new_attName);
+                
+                attName = new_attName;
+            }
+        
+            // 如果目标目录不存在，则创建目标目录
+            if (!this.app.vault.getAbstractFileByPath(destinationPath)) {
+                this.app.vault.createFolder(destinationPath);
+                console.log('目标路径不存在，创建目录：%s',destinationPath);
+            }
+        
+            // 移动文件
+            this.app.vault.copy(sourceFile,destinationPath);
+        
+            console.log(attName + ' moved successfully.');
+        } else {
+            console.error('The file does not exist.');
+        }
+        return destinationPath;
+    }
+
+    async findAttachments(text: string): Promise<string[]> {
+        // 匹配所有附件引用，![[]]用法,[[01-工作笔记/attachments/测试文件.pdf]]和markdown图片用法：![]()
+        const regex = /!\[\[([^\]]+)\]\]|!\[.*\]\((.*)\)|\[\[([^\]]+)\]\]/g;
+        const matches = text.matchAll(regex);
+        const attachments: string[] = [];
+        
+        for (const match of matches) {
+          // 获取匹配到的文件名
+          const fileName = match[1];
+          attachments.push(fileName);
+        }
+      
+        return attachments;
+      }
+
+    //判断是不是绝对路径,true-是绝对路径，false-相对路径
+    isAbsolutPath(attachmentsPath: string):boolean {
+        let flag = false;
+        const index = attachmentsPath.indexOf(this.settings.attachmentDir);
+        //obsidian相对路径一般有两种写法：1、"attachments/0000.png|200"；2、"0000.png|200"；其他的为绝对路径："01-工作笔记/attachments/0000.png"
+        if(index>=0){
+            flag = true;
+        }
+
+        return flag;
+    }
+
+
+    getDate(){
+        //三目运算符
+        const Dates = new Date();
+    
+        //年份
+        const Year : number = Dates.getFullYear(); 
+    
+        //月份下标是0-11
+        const Months : any = ( Dates.getMonth() + 1 ) < 10  ?  '0' + (Dates.getMonth() + 1) : ( Dates.getMonth() + 1); 
+    
+        //具体的天数
+        const Day : any = Dates.getDate() < 10 ? '0' + Dates.getDate() : Dates.getDate();
+    
+       //小时
+       const Hours = Dates.getHours() < 10 ? '0' + Dates.getHours() : Dates.getHours();
+    
+       //分钟
+       const Minutes = Dates.getMinutes() < 10 ? '0' + Dates.getMinutes() : Dates.getMinutes();
+    
+       //秒
+       const Seconds = Dates.getSeconds() < 10 ? '0' + Dates.getSeconds() : Dates.getSeconds();
+    
+       //返回数据格式
+       return Year + Months + Day + '_' + Hours + Minutes + Seconds; 
+   }
+      
 }
--- src/setting.ts
+++ src/setting.ts
@@ -1,14 +1,97 @@
-import { App, PluginSettingTab } from "obsidian"
-import VueSamplePlugin from "./main"
+import { App, PluginSettingTab,Notice,Setting } from "obsidian"
+import AttachmentsBindsPlugin from "./main"
 
-export class VueSamplePluginSettingTab extends PluginSettingTab {
-    plugin: VueSamplePlugin
-    constructor(app: App, plugin: VueSamplePlugin) {
+export interface AttachmentsBindsSettingsType {
+  enableRelativePathBind: boolean;
+  enableAbsolutePathBind: boolean;
+  attachmentDir:string;
+  attachmentIgnoreType:string;
+}
+
+export const attachmentsBindsSettings:AttachmentsBindsSettingsType = {
+  enableRelativePathBind: true,
+  enableAbsolutePathBind: false,
+  attachmentDir:'attachments',
+  attachmentIgnoreType:''
+}
+
+export class AttachmentsBindsSettingTab extends PluginSettingTab {
+    plugin: AttachmentsBindsPlugin
+    constructor(app: App, plugin: AttachmentsBindsPlugin) {
         super(app, plugin)
         this.plugin = plugin
     }
     display() {
         const { containerEl } = this
         containerEl.empty()
+
+        containerEl.createEl("h1", { text: "Attachments Binds" });
+
+        const basicDiv = containerEl.createEl("div");
+        basicDiv.createEl("h2", { text: "常规设置"});
+
+        //声明
+        const webdavLongDescDiv = basicDiv.createEl("div", {
+          cls: "settings-long-desc",
+        });
+        webdavLongDescDiv.createEl("p", {
+          text: "声明：本插件目前仅支持各笔记附件的父级文件夹名称相同，例如：每个附件都是放置在attachments文件夹下，且移动后附件引用方式不变。对于形如“![[01-工作笔记/attachments/0000.png|left]]”这种形式的应用，目前更新后会舍弃掉“|left”部分对附件属性的设置。",
+          cls: "base-disclaimer",
+        });
+
+        //相对路径设置
+        new Setting(basicDiv)
+        .setName("是否绑定附件")
+        .setDesc("绑定附件后，当移动笔记时，会判断当前笔记所在目录的附件目录中是否包含笔记中引用的附件，如果包含，则进行移动，不包含则默认为对其他附件的服用，不做处理。")
+        .addToggle((toggle) => {
+          toggle
+            .setValue(this.plugin.settings.enableRelativePathBind)
+            .onChange(async (val) => {
+              this.plugin.settings.enableRelativePathBind = val;
+              await this.plugin.saveSettings();
+              new Notice("绑定附件成功！");
+            });
+        });
+        
+        // //绝对路径设置
+        // new Setting(basicDiv)
+        // .setName("是否绑定绝对路径")
+        // .setDesc("绑定绝对路径后，当移动笔记时，附件也会移动到笔记所在目录的附件目录下。适用于使用绝对路径引用附件文件情况，例如：/生活笔记/a.md附件使用的连接：/生活笔记/attachments/a.png")
+        // .addToggle((toggle) => {
+        //   toggle
+        //     .setValue(this.plugin.settings.enableAbsolutePathBind)
+        //     .onChange(async (val) => {
+        //       this.plugin.settings.enableAbsolutePathBind = val;
+        //       await this.plugin.saveSettings();
+        //       new Notice("绑定附件成功！");
+        //     });
+        // });
+        
+        //配置附件文件夹名称
+        new Setting(containerEl)
+        .setName("附件文件夹名称：")
+        .setDesc("配置附件所在文件夹名称，【注意】此配置项是全局配置，适用所有笔记。")
+        .addText((value) => {
+          value
+            .setValue(this.plugin.settings.attachmentDir)
+            .onChange(async (value) => {
+              this.plugin.settings.attachmentDir = value;
+              await this.plugin.saveSettings();
+              new Notice("配置附件文件夹名称："+value);
+            });
+        });
+        //配置附件排除格式
+        new Setting(containerEl)
+        .setName("附件排除格式：")
+        .setDesc("配置了排除的附件格式后，移动笔记时，对应的附件格式应用会被忽略，【注意】此配置项是全局配置，适用所有笔记.支持正则表达式，多个附件格式使用英文分号(;)分割。")
+        .addText((value) => {
+          value
+            .setValue(this.plugin.settings.attachmentIgnoreType)
+            .onChange(async (value) => {
+              this.plugin.settings.attachmentIgnoreType = value;
+              await this.plugin.saveSettings();
+              new Notice("配置过滤附件格式："+value);
+            });
+        });
     }
 }
